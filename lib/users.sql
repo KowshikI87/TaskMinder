f33970e53cf2f14e30fd01614700a833db245711
KowshikI87 <kowshikislam8@gmail.com>
2023-01-15 02:01:41 -0500

    init

---
+++ lib/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password)
+  VALUES ('admin', '$2b$10$tx8orVAqtyA7CdaCK9rRVeW/VnKgESp4keVmbj/2QVL8SvdiK3IgW'),
+         ('developer', '$2b$10$RIwuQnP.NKc1gJ5Ef08.dOSX8ibuEnDMziqJGr925nRM0LRhvCFN.'),
+         ('somebody', 'knock-knock');
